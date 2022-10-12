fae2d6ff19b3986f507ce0be0a77a0cca81a7b34
PawelPalasinski <pawel.palasinski@gmail.com>
2022-10-12 23:55:55 +0200

    delete btn

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,38 +1,52 @@
-import { useSelector } from 'react-redux';
-import {
-  useGetContactsQuery,
-  useDeleteContactMutation,
-} from '../../features/api/apiSlice';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/tasks/operations';
+
+// import { useSelector } from 'react-redux';
+// import {
+//   useGetContactsQuery,
+//   useDeleteContactMutation,
+// } from '../../features/api/apiSlice';
 import styles from './ContactList.module.css';
 
 const ContactList = ({ children }) => {
-  const filtersContacts = (contacts, filter) =>
-    contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+  // const filtersContacts = (contacts, filter) =>
+  //   contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase())
+  //   );
 
-  const filter = useSelector(state => state.filter);
+  // const filter = useSelector(state => state.filter);
 
-  const { data: contacts = [] } = useGetContactsQuery();
-  const [deleteContact] = useDeleteContactMutation();
-  const contactsList = filtersContacts(contacts, filter);
+  // const { data: contacts = [] } = useGetContactsQuery();
+  // const [deleteContact] = useDeleteContactMutation();
+  // const contactsList = filtersContacts(contacts, filter);
+
+  const dispatch = useDispatch();
+
+  const handleDelete = () => dispatch(deleteContact(id));
 
   return (
     <div className={styles.contacts}>
       {children}
       <h2>Contacts</h2>
       <ul>
-        {contactsList.map(({ name, phone, id }) => (
-          <li key={id} id={id}>
-            <p>
-              <span>{name} : </span>
-              {phone}
-            </p>
-            <button type="button" onClick={() => deleteContact({ id })}>
-              ⛌
-            </button>
-          </li>
-        ))}
+
+        <li>
+          <p>
+            <span>
+              {name}: {number}
+            </span>
+          </p>
+          <button
+            type="button"
+            className={btn}
+            onClick={() => {
+              dispatch(deleteContact(id));
+            }}
+          >
+            ⛌
+          </button>
+        </li>
+
       </ul>
     </div>
   );
